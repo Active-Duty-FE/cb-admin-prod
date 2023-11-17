2cabf3581b8883701242f7e224ffc9e7ef1b59ca
hyanghee2020 <10122599+hyanghee2020@user.noreply.gitee.com>
2023-11-17 22:22:48 +0900

    re commit

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,5 @@
 import { AppRequest } from './axios'
 
 export const appRequest = new AppRequest({
-  baseURL: '141.164.35.83:8888/api/private/v1'
+  baseURL: 'http://141.164.35.83:8888/api/private/v1'
 })
